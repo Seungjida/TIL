063002743638c2b28460dda40ed80456e2871d87
이승지 <esg0107@gmail.com>
2024-01-22 17:51:53 +0900

    파이썬 5일차 학습

---
+++ 02_python/python01/python_ws_5_5/ws_5_5.py
@@ -0,0 +1,15 @@
+# import copy
+# # 아래 함수를 수정하시오.
+# def even_elements(original_list):
+#     odd_list = []
+#     even_list = []
+#     for element in original_list:
+#         if element % 2 != 0:
+#             index = original_list.index(element)
+#             odd_list.append(original_list.pop(index))
+#     even_list.extend(original_list)
+#     return even_list
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# result = even_elements(my_list)
+# print(result)
