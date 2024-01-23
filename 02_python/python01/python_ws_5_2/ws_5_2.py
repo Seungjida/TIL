255f164023dcb75a80ee1173aef63b2e17731a4f
이승지 <esg0107@gmail.com>
2024-01-23 09:32:06 +0900

    for와 not in을 활용하여리스트의 중복요소 제거하기

--- 02_python/python01/python_ws_5_2/ws_5_2.py
+++ 02_python/python01/python_ws_5_2/ws_5_2.py
@@ -1,8 +1,17 @@
-# 아래 함수를 수정하시오.
-def remove_duplicates(target_list):
-    new_lst = list(set(target_list))
+# set()쓴 방법
+# def remove_duplicates(target_list):
+#     new_lst = list(set(target_list))
 
+#     return new_lst
+
+# for문으로 요소들 확인한 방법
+def remove_duplicates(target_list):
+    new_lst = []
+    for element in target_list:
+        if element not in new_lst:
+            new_lst.append(element)
     return new_lst
+
 
 
 result = remove_duplicates([1, 2, 2, 3, 4, 4, 5])
