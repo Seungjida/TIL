f762cceb95086ea002bcca5ff0a4b281127d0998
이승지 <esg0107@pusan.ac.kr>
2024-03-28 16:01:04 +0900

    장고 학습한 파일 업로드

---
+++ django/07/static, media 학습/articles/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
+
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+def create(request):
+    if request.method == 'POST':
+        # 다른 곳에 저장되어 있으므로 FILES도 인자(basemodelform의 생성자 함수 키워드 인자 보면 data 다음 file 나옴)로 넘겨야 함
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form': form,
+        'article': article,
+    }
+    return render(request, 'articles/update.html', context)
